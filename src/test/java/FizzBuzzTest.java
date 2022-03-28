b4da7b286f783b22f8e77c1889cd853e536e8531
Wolfero <ferorock44@gmail.com>
2022-03-29 00:24:00 +0100

    🧪Test: given_number_return_number_in_string_format

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FizzBuzzTest {
+    @Test
+    void given_number_return_number_in_string_format() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        String result = fizzBuzz.checkEquivalent(1);
+
+        assertThat(result).isEqualTo("1");
+    }
 }
