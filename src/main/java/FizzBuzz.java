f4ab3bd98c601621f28c672da21b69a463e1a13a
Wolfero <ferorock44@gmail.com>
2022-03-29 00:26:43 +0100

    ✅Test: given_multiple_of_five_return_buzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,7 @@
 public class FizzBuzz {
     public String checkEquivalent(int givenNumber) {
         if(givenNumber % 3 == 0)return "Fizz";
+        if (givenNumber % 5 == 0) return "Buzz";
         return Integer.toString(givenNumber) ;
     }
 }
