b91fcdfbdd7c39c7219d2dafc75860ea28b761e0
Wolfero <ferorock44@gmail.com>
2022-03-29 00:25:36 +0100

    ✅Test: given_multiple_of_three_return_fizz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,6 @@
 public class FizzBuzz {
     public String checkEquivalent(int givenNumber) {
+        if(givenNumber % 3 == 0)return "Fizz";
         return Integer.toString(givenNumber) ;
     }
 }
